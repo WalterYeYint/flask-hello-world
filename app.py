08cac2e600c79475040e61b5a02af842d2cbced1
Kan Yu <33181889+WalterYeYint@users.noreply.github.com>
2023-10-15 22:00:11 +0630

    add get_data() for test;

--- app.py
+++ app.py
@@ -4,3 +4,8 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    data = {"message": "Hello from Python!"}
+    return jsonify(data), 200
